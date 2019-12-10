4016a8f6a6c8ebc7daf1c7cc0dd150dcbff52d37
MattyMoot <matt.moot@outlook.com>
2019-12-11 10:46:39 +1300

    final commit

--- src/components/Shapes.js
+++ src/components/Shapes.js
@@ -1,40 +1,69 @@
-import React from 'react';
+import React, { Component, useState, } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from '../App';
-import * as serviceWorker from '../serviceWorker';
+
+
 
 
 class Shapes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            bgColor: "",
+            bgColor: "#77b3d1",
             size: 100,
-            perCent: "50%",
+            perCent: 50,
         }
+        this.changeColor = this.changeColor.bind(this);
+        this.changeSize = this.changeSize.bind(this);
+        this.changeShape = this.changeShape.bind(this);
     }
+
+
+
+    //generates a random number for the width/height for each shape
+    GenerateRandomWH = () => {
+        var RandomNumber = Math.floor(Math.random() * 100) + 1;
+        this.setState({
+            NumberHolder: RandomNumber
+        })
+    }
+
 
 
     changeColor() {
         let randomIndexColor = Math.floor(Math.random() * 19 + 0)
-        this.setState({ bgColor: this.colors[randomIndexColor] })
-    }
-
-
-    render() {
         var colors = ["#77b3d1", "#E94F37", "#1C89BF", "#A1D363", "#85FFC7", "#297373", "#FF8552",
             "#A40E4C", "#85AFC0", "#296573", "#AA8552", "#CC0E4C", "#567187", "#c6c976", "#967c64",
             "#e497ed", "#d24cff", "#e2ceb1", "#cc999e", "#97bf9a"];
+        this.setState({ bgColor: colors[randomIndexColor] });
+
+    }
+    changeSize() {
+        let randomIndexSize = Math.floor(Math.random() * 125 + 0)
+        this.setState({ size: randomIndexSize });
+    }
+    changeShape() {
+        let randomIndexShape = Math.floor(Math.random() * 100 + 0)
+        this.setState({ perCent: randomIndexShape });
+    }
+    componentDidMount() {
+        setInterval(this.changeColor, 1000);
+        setInterval(this.changeSize, 1000)
+        setInterval(this.changeShape, 1000)
+    }
+
+
+
+    render() {
+
 
         var circleStyle = {
             padding: 10,
             margin: 20,
             display: "inline-block",
-            backgroundColor: this.props.bgColor,
-            borderRadius: "50%",
-            width: 100,
-            height: 100
+            backgroundColor: this.state.bgColor,
+            borderRadius: this.state.perCent,
+            width: this.state.size,
+            height: this.state.size
         };
 
         return (
@@ -43,6 +72,19 @@ class Shapes extends React.Component {
         );
     }
 }
+
+
+
+function counter() {
+    const element = (
+        <div>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+//setInterval(tick, 1000);
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//import Shapes from './components/Shapes';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
